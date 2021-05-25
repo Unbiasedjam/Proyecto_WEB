43bba0e979d4df3d6035b360581c7833c612e035
Unbiasedjam <36214022+Unbiasedjam@users.noreply.github.com>
2021-05-25 10:44:37 -0700

    Agregado otra pagina para el login
    Agregue otro inicio de sesion para capturar usuarios y la "pagina principal"

--- admin.php
+++ admin.php
@@ -13,15 +13,15 @@ if(!$conn)
 
 $nombre = $_POST["txtUser"];
 $pass = $_POST["txtPass"];
+$nombres2 = $_POST["usuarios"];
+$pass2 = $_POST["contrasenas"];
 
 $admin = mysqli_query($conn, "SELECT * FROM tbl_admin WHERE User_ID = '".$nombre."' AND password = '".$pass."'");
 $nr = mysqli_num_rows($admin);
 
-
-
 if($nr == 1)
 {
-    echo "Bienvenido: " .$nombre; //cambiar a pagina para admin
+    header("Location: pages/Capturar_Usuarios.html"); //cambiar a pagina para admin
 }
 else if ($nr == 0)
 {
@@ -29,5 +29,16 @@ else if ($nr == 0)
 }
 
 
+$users = mysqli_query($conn, "SELECT * FROM tbl_users WHERE User_ID = '".$nombres2."' AND password = '".$pass2."'");
+$rn = mysqli_num_rows($users);
+
+if($rn == 1)
+{
+    header("Location: main.html"); //cambiar a pagina para admin
+}
+else if ($rn == 0)
+{
+    header("Location: login.html");
+}
 
 ?>
